da8c74b2c4d798ac7dd23da48d5453a45ab960d2
WasifSohail5 <wasifsohail66@gmail.com>
2026-04-23 15:20:14 +0500

    chore: add chrome options in backend/lecture.py

--- backend/lecture.py
+++ backend/lecture.py
@@ -190,7 +190,14 @@ TEMPLATES = {
 
 def capture_all_slides_integrated(html_file, output_folder="exported_frames"):
     chrome_options = Options()
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--disable-gpu")
     chrome_options.add_argument("--headless=new")
+    chrome_options.add_argument("--window-size=1920,1080")
+    chrome_options.add_argument("--disable-setuid-sandbox")
+    chrome_options.add_argument("--remote-debugging-port=9222")
+    chrome_options.add_argument("--disable-extensions")
 
     driver = webdriver.Chrome(options=chrome_options)
     image_paths = []
